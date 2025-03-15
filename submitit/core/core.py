29e6b6c9bedac11e118b48bccf143e1b65734bbf
Hadar Shavit <shavit.hadar@gmail.com>
2025-03-15 10:31:26 +0100

    Update core.py

--- submitit/core/core.py
+++ submitit/core/core.py
@@ -746,7 +746,7 @@ class Executor(abc.ABC):
             logger.get_logger().info(f"Jobs in queue: {len(self._delayed_batch)}")
             states = set([j.state for j in jobs_in_queue])
             logger.get_logger().info(f"States: {states}")
-            jobs_in_queue = [j for j in jobs_in_queue if j.state == 'PENDING' or j.state == 'UNKNOWN']
+            jobs_in_queue = [j for j in jobs_in_queue if j.state in ['PENDING', 'UNKNOWN', 'RUNNING']]
             current_batch_size = min(len(self._delayed_batch), max_queue_jobs - len(jobs_in_queue))
             if current_batch_size >= min_array_size or len(self._delayed_batch) <= min_array_size:
                 current_batch = self._delayed_batch[:current_batch_size]
